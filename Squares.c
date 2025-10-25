e25883756d068e90fc8592f2b006808217b753d0
MourningOwl <alioudibassy50@gmail.com>
2025-10-24 21:00:14 -0400

    Addition of a user check at 25 squares entered

--- Squares.c
+++ Squares.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
 
-int main(){
-
-int num;
-
-printf("enter a positive number to get even squares: ");
-
-scanf("%d", &num);
-
-for(int i = 1; i < num+1; i++){
-
-	if (i%2){
-		continue;
-	}
-
-	for(int j = 1; (j * j) < (num + 1); j++) {
-		if (j * j == i && j * j % 2 == 0) {
-			printf("%d\n", i);
-			break;
-		}
+int user(char square) {
+	while (getchar() != '\n') {
 
 	}
 
-
+	return 0;
 }
 
 
+int main(){
 
+	char square = 0;
+
+	long int num;
+
+	printf("enter a positive number to get even squares: ");
+
+	scanf("%ld", &num);
+
+	for(int i = 1; i < num+1; i++){
+		if (i % 2){
+			continue;
+		}
+
+		for(int j = 1; (j * j) < (num + 1); j++) {
+			if (square > 24) {
+				printf("Please press ENTER to continue...");
+				square = user(square); //return 0
+		}
+
+		if (j * j == i ) {
+			square++;
+			printf("%d\n", i);
+			break;
+		}
+	}
+}
 }
